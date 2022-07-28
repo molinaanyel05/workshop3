9b8e02b5be6a887f9158e4a094687bbf0cddf47e
kevccruz1999 <86648828+kevccruz1999@users.noreply.github.com>
2022-07-27 21:35:43 -0600

    Added dotenv

--- src/mongoose.js
+++ src/mongoose.js
@@ -1,23 +1,19 @@
 const mongoose = require("mongoose");
-
+require("dotenv").config();
 
 const Album = require("./Models/albumModel");
 
 //usar usuario y contraseña creados y agregar el nombre de la BD despues de ".net/"
-const url ="mongodb+srv://user:haD3MC6290UQBDhz@cluster0.0oslmrk.mongodb.net/proyecto2";
+const url = process.env.MONGO_DB;
 
-const connectdb = async()=>{
+const connectdb = async () => {
   try {
-    await mongoose.connect(url,{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  
+    await mongoose.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-  console.log("Connected to DB!!!");
-  } catch (error) {
-    
-  }
-}
-module.exports= connectdb;
-
+    console.log("Connected to DB!!!");
+  } catch (error) {}
+};
+module.exports = connectdb;
