9fd631efabf8b65ab634957c9f4b5a5c853f7bd9
molinaanyel05 <molinanyel05@gmail.com>
2022-08-17 17:06:25 -0600

    cambios}

--- src/Routes/albumsRouter.js
+++ src/Routes/albumsRouter.js
@@ -14,18 +14,21 @@ router.post("/album",auth,
  albumController.createAlbum
 );
 
-router.get("/albums/:user", auth,albumController.showAlbums);
+router.get("/albums/:user", albumController.showAlbums);
 
 router.get("/album/:id", albumController.getAlbumById);
 
-router.get("/album/:user/:title", 
-[
-    check("user").not().isEmpty().withMessage("user is required"),
-    check("title").not().isEmpty().withMessage("title is required"),
-], 
-albumController.getAlbumByName);
+router.get("/album/:user/:title", albumController.getAlbumByName);
 
-router.put("/album/:id",auth,albumController.updateAlbum);
+router.put("/album/:id",auth,
+[
+    check("name").not().isEmpty().withMessage("Name is required"),
+    check("description").not().isEmpty().withMessage("Description is required"),
+    validateFields
+],
+albumController.updateAlbum);
+
+
 router.delete("/album/:id",auth,albumController.deleteAlbum);
 
 module.exports = router;
