d5ba91aa16bfd231e510c4579fddce8d91f92465
kevccruz1999 <86648828+kevccruz1999@users.noreply.github.com>
2022-08-17 17:17:37 -0600

    Solved all

--- src/Routes/albumsRouter.js
+++ src/Routes/albumsRouter.js
@@ -1,31 +1,44 @@
 const express = require("express");
 const { check } = require("express-validator");
 const auth = require("../middlewares/auth");
-const {validateFields} = require("../middlewares/validate-fields");
+const { validateFields } = require("../middlewares/validate-fields");
 const albumController = require("./../Controllers/albumsController");
 const router = express.Router();
 
-router.post("/album",auth,
-[
+router.post(
+  "/album",
+  auth,
+  [
     check("name").not().isEmpty().withMessage("Name is required"),
     check("description").not().isEmpty().withMessage("Description is required"),
-    validateFields
-],
- albumController.createAlbum
+    validateFields,
+  ],
+  albumController.createAlbum
 );
 
-router.get("/albums/:user", auth,albumController.showAlbums);
+router.get("/albums/:user", auth, albumController.showAlbums);
 
 router.get("/album/:id", albumController.getAlbumById);
 
-router.get("/album/:user/:title", 
-[
+router.get(
+  "/album/:user/:title",
+  [
     check("user").not().isEmpty().withMessage("user is required"),
     check("title").not().isEmpty().withMessage("title is required"),
-], 
-albumController.getAlbumByName);
+  ],
+  albumController.getAlbumByName
+);
 
-router.put("/album/:id",auth,albumController.updateAlbum);
-router.delete("/album/:id",auth,albumController.deleteAlbum);
+router.put(
+  "/album/:id",
+  auth,
+  [
+    check("name").not().isEmpty().withMessage("Name is required"),
+    check("description").not().isEmpty().withMessage("Description is required"),
+    validateFields,
+  ],
+  albumController.updateAlbum
+);
+router.delete("/album/:id", auth, albumController.deleteAlbum);
 
 module.exports = router;
